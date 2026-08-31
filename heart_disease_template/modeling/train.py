120dca132614a009839013f155f0740352deed99
kiran-21 <kiranksr.sr@gmail.com>
2026-08-31 17:54:04 -0400

    features.py update

--- heart_disease_template/modeling/train.py
+++ heart_disease_template/modeling/train.py
@@ -2,33 +2,24 @@ from pathlib import Path
 from loguru import logger
 from tqdm import tqdm
 import typer
+import pandas as pd
 
-from heart_disease_template.config import MODELS_DIR, PROCESSED_DATA_DIR, RAW_DATA_DIR
+from heart_disease_template.config import MODELS_DIR, PROCESSED_DATA_DIR,INTERIM_DATA_DIR
 
 app = typer.Typer()
 
 
 @app.command()
-# def main(
-#     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-#     features_path: Path = RAW_DATA_DIR / "features.csv",
-#     labels_path: Path = RAW_DATA_DIR / "labels.csv",
-#     model_path: Path = MODELS_DIR / "model.pkl",
-#     # -----------------------------------------
-# ):
-#     # ---- REPLACE THIS WITH YOUR OWN CODE ----
-#     logger.info("Training some model...")
-#     for i in tqdm(range(10), total=10):
-#         if i == 5:
-#             logger.info("Something happened for iteration 5.")
-#     logger.success("Modeling training complete.")
+def main(
+    features_path: Path = PROCESSED_DATA_DIR / "features_heart.csv",
+    # labels_path: Path = RAW_DATA_DIR / "labels.csv",
+    model_path: Path = MODELS_DIR / "model.pkl",
     # -----------------------------------------
-
-def main():
-    print("hello world from train file")
-    return None
+):
+    df=pd.read_csv(features_path)
     
-
-
+    # print(df.head(5))
+    print("model Training.......")
+    
 if __name__ == "__main__":
     app()
--- heart_disease_template/plots.py
+++ heart_disease_template/plots.py
@@ -1,10 +1,14 @@
 from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
 
 from loguru import logger
 from tqdm import tqdm
 import typer
 
-from heart_disease_template.config import FIGURES_DIR, PROCESSED_DATA_DIR
+from heart_disease_template.config import FIGURES_DIR, PROCESSED_DATA_DIR, INTERIM_DATA_DIR
 
 app = typer.Typer()
 
@@ -12,17 +16,35 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
+    input_path: Path = PROCESSED_DATA_DIR / "features_heart.csv",
+    interim_path: Path = INTERIM_DATA_DIR/"interim_data_heart.csv",
     output_path: Path = FIGURES_DIR / "plot.png",
     # -----------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating plot from data...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Plot generation complete.")
-    # -----------------------------------------
+   print("Plot and figures..")
+   df=pd.read_csv(input_path)
+   df2=pd.read_csv(interim_path)
+   correlation_matrix(df)
+   heart_disease(df2)
+
+
+def correlation_matrix(df):
+    print("checking correlation of data....")
+    corr = df.corr()
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+    plt.figure(figsize=(14, 14))
+    sns.heatmap(corr, mask=mask, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+    plt.title("Heatmap", fontsize=16,color="darkblue",fontweight="bold")
+    plt.savefig(FIGURES_DIR / "Feature_Correlation_Heatmap.png")
+    plt.close()
+
+def heart_disease(df2):
+    print("checking the heart disease count based on gender")
+    plt.figure(figsize=(12, 8))
+    sns.countplot(data=df2, x='HeartDisease', hue='Sex', palette='Set1')
+    plt.title("Heart Disease Count by Sex")
+    plt.savefig(FIGURES_DIR / "Heart_Disease_Count_by_Sex.png")
+    plt.close()
 
 
 if __name__ == "__main__":
